a11ce1f14e9c2564af19f4e97bd54a542b148e91
Osana AD <austin_agbo@yahoo.com>
2026-07-22 09:35:42 +0000

    commit message stor

--- Xchange/capacitor.config.ts
+++ Xchange/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Xchange',
+  appName: 'Doustor',
   webDir: 'dist'
 };
 
